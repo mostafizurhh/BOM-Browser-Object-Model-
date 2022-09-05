0e2e492d9033197e9ae6beebfc5ba6924d064ba8
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-05 23:29:05 +0100

    explored alert vs confirm vs prompt

--- alert,confirm,prompt/browserModal.js
+++ alert,confirm,prompt/browserModal.js
@@ -1,1 +1,35 @@
-console.log('hi')
+function showAlert() {
+    const num = Math.random() * 10;
+    console.log(num)
+    if (num < 5) { alert('this is an alert message from browser') }
+}
+
+function showConfirm() {
+    const num = Math.random() * 100;
+    console.log(num);
+    if (num < 20) {
+        const answer = confirm('do you want to proceed?');
+        console.log(answer)
+        if (!!answer) {
+            const answer2 = confirm('press cancel')
+            console.log(answer2)
+        }
+        return answer;
+    }
+}
+
+function showPrompt() {
+    const ans = prompt('enter your name')
+    console.log(ans)
+    if (!!ans) {
+        const ans1 = prompt('enter your email')
+        console.log(ans1)
+        if (!!ans1) {
+            alert('thanks')
+        }
+    }
+    else {
+        alert('please put your info')
+        prompt('enter your name')
+    }
+}
